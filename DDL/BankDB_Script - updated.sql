a586a4e0fa60620e2cf4af0c827c995627be1a4d
dsasidharannair <105741564+dsasidharannair@users.noreply.github.com>
2022-11-26 16:26:54 -0600

    ;jg4e;

--- DDL/BankDB_Script - updated.sql
+++ DDL/BankDB_Script - updated.sql
@@ -20,6 +20,7 @@ Create table Branch(
  	Status varchar(8),
 	Address_Id varchar(10) references Address
 	--include check for status and branch_Id
+	check(Status='Active' or Status='Closed')
 	);
 
 Create table Customer(
@@ -32,10 +33,12 @@ Create table Customer(
 
 Create table Account(
 	Account_Id varchar(10) primary key,
-	Type varchar(10),
-	Balance money,
+	Type varchar(2),
+	Balance numeric(13,3),
 	Customer_Id varchar(10) references Customer
 	--include check statement for Type and balance
+	check(balance>0),
+	check(type='C' or type='S')
 	);
 
 Create table Employee(
@@ -43,7 +46,7 @@ Create table Employee(
 	Name varchar(30),
 	Position varchar(20),
 	SSN char(9),
-	Salary money,
+	Salary numeric(10,2),
 	Address_Id varchar(10) references Address,
 	Branch_Id varchar(10) references Branch
 	--include check for employee id, ssn
@@ -51,12 +54,13 @@ Create table Employee(
 
 Create table Transactions(
 	Transaction_Id varchar(10) primary key,
-	Type varchar(10),
-	Amount money,
+	Type varchar(20),
+	Amount numeric(13,3),
 	Description varchar(30),
 	Customer_Id varchar(10) references Customer,
 	Employee_Id varchar(10) references Employee
 	--include check for amount and customer,employee
+	check(Type='Deposit' or Type='Withdrawl' or Type='Transfer')
 	);
 
 Create table to_from(
--- DML/Values.sql
+++ DML/Values.sql
@@ -1,11 +1,11 @@
 -- Creating initial adresses
-INSERT INTO Address VALUES('ADDB7F8224', 'Chicago', 'IL', '60653');
-INSERT INTO Address VALUES('AD9A4687AC', 'Chicago', 'IL', '60639');
+INSERT INTO Address VALUES('1', 'Chicago', 'IL', '60653');
+INSERT INTO Address VALUES('2', 'Chicago', 'IL', '60639');
 
 -- Creating initial branches
-INSERT INTO Branch VALUES('BR44122B41', 'Active', 'ADDB7F8224');
-INSERT INTO Branch VALUES('BRE37AB30F', 'Active', 'AD9A4687AC');
+INSERT INTO Branch VALUES('1', 'Active', '1');
+INSERT INTO Branch VALUES('2', 'Active', '2');
 
 -- Creating employees
-INSERT INTO employee VALUES('EMD76A86FC', 'Achraf Kamni', 'Manager', '509204145', 150000.00, 'ADDB7F8224', 'BR44122B41');
-INSERT INTO employee VALUES('EMBE09F2D2', 'Sasidharan Nair Darshan', 'Teller', '247881067', 50000.00, 'AD9A4687AC', 'BRE37AB30F');
+INSERT INTO employee VALUES('1', 'Achraf Kamni', 'Manager', '509204145', 150000.00, '1', '1');
+INSERT INTO employee VALUES('2', 'Sasidharan Nair Darshan', 'Teller', '247881067', 50000.00, '2', '2');
