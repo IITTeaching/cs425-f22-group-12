8930aaf3f13eb36080d9c07f4e6da607507361c1
dsasidharannair <105741564+dsasidharannair@users.noreply.github.com>
2022-11-26 16:21:03 -0600

    Update main.py

--- BankApplication/main.py
+++ BankApplication/main.py
@@ -1,1 +1,22 @@
-print("hello world")
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="project",
+    user="postgres",
+    password="")
+cur=conn.cursor()
+def ex():
+    try:
+        cur.execute("insert into m values(2,'bye');")
+        print("it worked")
+        cur.close()
+        conn.commit()
+    except(Exception,psycopg2.DatabaseError) as e:
+        print(e)
+    finally:
+        if conn is not None:
+            conn.close()
+            print("Connection closed")
+
+
